0315f138700f3ee929b6c49ee7fc73950baf6779
Akshay9607 <akshay96070418@gmail.com>
2023-03-03 20:49:31 +0530

    Initialised project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "FarmHelp",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.green[800],
+      ),
+    ),
+  ));
+}
