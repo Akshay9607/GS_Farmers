d1f0ebb7f98eedc4fd1ca705afae6d56a7a03507
rajkumardongre <rajkumardongre17@gmail.com>
2023-03-19 03:48:28 +0530

    Added Education Category Screen

---
+++ lib/education/category.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+
+void main() => runApp(MaterialApp(
+  home : EducationCategory(),
+));
+
+class EducationCategory extends StatelessWidget {
+  const EducationCategory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(212, 255, 208, 1),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Search box
+            Container(
+              padding: EdgeInsets.all(30.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color.fromRGBO(146, 212, 145, 1),
+              ),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Educate,",
+                    style: TextStyle(
+                      color: Colors.white, // Sets the color to blue
+                      fontSize: 25.0, // Sets the font size to 16.0 pixels
+                      fontWeight: FontWeight.bold, // Sets the font weight to bold
+                    ),
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    "yourself here",
+                    style: TextStyle(
+                      color: Colors.white, // Sets the color to blue
+                      fontSize: 20.0, // Sets the font size to 16.0 pixels
+                      fontWeight: FontWeight.w100, // Sets the font weight to bold
+                    ),
+                  ),
+                  SizedBox(height: 30.0),
+                  TextField(
+                    style: TextStyle(
+                      color: Color.fromRGBO(183, 183, 183, 1), // Sets the text color to black
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Enter your name',
+                      hintStyle: TextStyle(
+                        color: Color.fromRGBO(197, 197, 199, 1), // Sets the hint text color to grey
+                      ),
+                      filled: true, // Fills the background with a color
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0), // Sets a border radius of 30.0 pixels
+                        borderSide: BorderSide.none, // Removes the border line
+                      ),
+                      suffixIcon: Icon(
+                        Icons.search, // Sets the search icon as the suffix icon
+                        color: Color.fromRGBO(183, 183, 183, 1),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(horizontal: 20.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30.0),
+            // Education Category Title
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                "Education categories",
+                style: TextStyle(
+                  color: Color.fromRGBO(29, 101, 4, 1), // Sets the color to blue
+                  fontSize: 25.0, // Sets the font size to 16.0 pixels
+                  fontWeight: FontWeight.bold, // Sets the font weight to bold
+                ),
+              ),
+            ),
+            SizedBox(height: 24.0),
+            Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: GridB(),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Container(
+            height: 50.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.home_outlined),
+              Icon(Icons.book_outlined),
+              Icon(Icons.store_mall_directory_outlined),
+              Icon(Icons.image_search ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GridB extends StatefulWidget {
+  const GridB({Key? key}) : super(key: key);
+
+  @override
+  State<GridB> createState() => _GridBState();
+}
+
+class _GridBState extends State<GridB> {
+  final List<Map<String, dynamic>> gridMap = [
+    {
+      "title" : "Crops",
+      "imageSrc" : "assets/education/crops.jpg"
+    },
+    {
+      "title" : "Crop Management",
+      "imageSrc" : "assets/education/crop_management.jpg"
+    },
+    {
+      "title" : "Weather Forecast",
+      "imageSrc" : "assets/education/livestock.jpg"
+    },
+    {
+      "title" : "Livestock",
+      "imageSrc" : "assets/education/rainy.png"
+    }
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+        mainAxisExtent: 170.0,
+      ),
+      itemCount: gridMap.length,
+      itemBuilder: (_, index){
+      return Container(
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12.0),
+          color: Colors.white
+        ),
+        child: Column(
+          children: [
+            Image(
+              image: AssetImage(gridMap.elementAt(index)['imageSrc']),
+              width: 150.0,
+              height: 110.0,
+            ),
+            Text(
+              gridMap.elementAt(index)['title'],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Color.fromRGBO(29, 101, 4, 1), // Sets the color to blue
+                  fontSize: 15.0, // Sets the font size to 16.0 pixels
+                  fontWeight: FontWeight.bold, // Sets the font weight to bold
+                ),
+            )
+          ],
+        ),
+      );
+    });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
+import 'package:farmhelp/education/category.dart';
 import 'package:flutter/material.dart';
-
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-            "FarmHelp",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.green[800],
-      ),
-    ),
+  runApp(
+      MaterialApp(
+      home: EducationCategory(),
   ));
 }
